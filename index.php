af76e6a3ea0a563a8247e1f27e23f63acc715c73
thomasehuang <thomas.e.huang@hotmail.com>
2016-02-27 04:40:52 -0500

    added utility.php and changed index.php

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
 
+require_once "utility.php";
+
 echo "Hello, World!";
+
+// Assumes database already exists.
+$conn = connect();
+$sql = "CREATE TABLE items(
+			id INT NOT NULL AUTO_INCREMENT, 
+			PRIMARY KEY(id),
+			name VARCHAR(30),
+			category VARCHAR(30),
+			date DATE,
+			is_complete  BOOL)";
+try{
+	$conn->query($sql);
+}
+catch(Exception $e){
+	print_r($e);
+}
+echo "<h3>Table created.</h3>";
 
 ?>
