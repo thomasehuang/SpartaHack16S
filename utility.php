aa1f9edc772648c3ba4d2fbc3c51ac211ca36453
thomasehuang <thomas.e.huang@hotmail.com>
2016-02-27 04:48:44 -0500

    fixed utility.php

--- utility.php
+++ utility.php
@@ -29,4 +29,25 @@ function connect() {
 	return $conn;
 }
 
+/**	* Example of creating a table
+
+// Assumes database already exists.
+$conn = connect();
+$sql = "CREATE TABLE items(
+			id INT NOT NULL AUTO_INCREMENT, 
+			PRIMARY KEY(id),
+			name VARCHAR(30),
+			category VARCHAR(30),
+			date DATE,
+			is_complete  BOOL)";
+try{
+	$conn->query($sql);
+}
+catch(Exception $e){
+	print_r($e);
+}
+echo "<h3>Table created.</h3>";
+
+*/
+
 ?>
