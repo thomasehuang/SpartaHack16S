b83bea7fd77e47cc3b55a009b504f35a6e5b25a8
thomasehuang <thomas.e.huang@hotmail.com>
2016-02-27 04:49:09 -0500

    changed both files so it will work

--- index.php
+++ index.php
@@ -4,21 +4,6 @@ require_once "utility.php";
 
 echo "Hello, World!";
 
-// Assumes database already exists.
-$conn = connect();
-$sql = "CREATE TABLE items(
-			id INT NOT NULL AUTO_INCREMENT, 
-			PRIMARY KEY(id),
-			name VARCHAR(30),
-			category VARCHAR(30),
-			date DATE,
-			is_complete  BOOL)";
-try{
-	$conn->query($sql);
-}
-catch(Exception $e){
-	print_r($e);
-}
-echo "<h3>Table created.</h3>";
+echo "End, World!";
 
 ?>
