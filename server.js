eab0a4e181cba16b81b4846c0642d6c1214cbb46
Anthony <Anthony@14-144-184.guest.wireless.msu.edu>
2016-02-27 21:13:39 -0500

    work

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var http = require('http')
+var url = require("url");
+
 var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    res.end('Hello World\n');
-}).listen(port);
+
+function start(route, handle) {
+    function onRequest(request, response) {
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " received.");
+        route(handle, pathname, response, request);
+    }
+
+    http.createServer(onRequest).listen(port);
+}
+
+exports.start = start;
