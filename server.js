138284c0edac9dab55e125ba0ad9d6186df52838
Thomas Huang <thomas.e.huang@hotmail.com>
2016-02-27 20:54:26 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,8 @@
 var http = require('http')
-var url = require("url");
-
 var port = process.env.PORT || 1337;
-
-function start(route, handle) {
-    function onRequest(request, response) {
-        var pathname = url.parse(request.url).pathname;
-        console.log("Request for " + pathname + " received.");
-        route(handle, pathname, response, request);
-    }
-
-    http.createServer(onRequest).listen(port);
-}
-
-exports.start = start;
-
-// var http = require('http')
-// var port = process.env.PORT || 1337;
-// http.createServer(function(req, res) {
-//     res.writeHead(200, {
-//         'Content-Type': 'text/plain'
-//     });
-//     res.end('Hello World\n');
-// }).listen(port);
+http.createServer(function(req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    res.end('Hello World\n');
+}).listen(port);
